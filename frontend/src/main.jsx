27009c36c242b1f955ba27306b43fdaae8bb9821
Ridhima Gogia <gogia.ridhima@gmail.com>
2026-09-18 21:33:37 +0530

    Fix App.jsx filename casing for Vite

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./App.css";
 
-import App from "./App.jsx";
+import App from './App';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
