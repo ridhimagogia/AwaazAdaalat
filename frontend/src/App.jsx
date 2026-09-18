dca8188e96bb19789186ede65d05336cc0df0905
Ridhima Gogia <gogia.ridhima@gmail.com>
2026-09-18 21:38:48 +0530

    Fix casing and path for Home component import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
+import Home from "./pages/Home.jsx";
 import Upload from "./pages/Upload";
 import Result from "./pages/Result";
 import Explanation from "./pages/Explanation";
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./App.css";
 
-import App from './App';
+import App from './App.jsx';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
